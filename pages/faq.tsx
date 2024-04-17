1b8e06a46f375a942e3de28dcefd5ca0af5775d8
iotaben <80335737+iotaben@users.noreply.github.com>
2024-04-17 10:07:01 +0200

    Update faq.tsx

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -19,7 +19,7 @@ const FAQ: React.FC = () => (
         <ContentBox
           image='http://assets.iotabots.io/compressed/11.png'
           // eslint-disable-next-line max-len
-          text='An IOTABOT is a NFT in the form of a profile picture (PFP) that gives you access to the IOTABOTVERSE. They were the first NFTs (ERC721 Tokens) on the public IOTA Smart Contract EVM Testnet in 2021, now they are deployed on the ShimmerEVM Mainnet.'
+          text='An IOTABOT is a NFT in the form of a profile picture (PFP). They were the first NFTs (ERC721 Tokens) on the public IOTA Smart Contract EVM Testnet in 2021, now they are deployed on the ShimmerEVM Mainnet.'
           headline='WHAT IS AN IOTABOT?!'
         >
           {}
@@ -27,16 +27,8 @@ const FAQ: React.FC = () => (
         <ContentBox
           image='http://assets.iotabots.io/compressed/987.png'
           // eslint-disable-next-line max-len
-          text='There are 1337 IOTABOTS, 1001 SPACEBOTS and 3333 SOONABOTS.'
+          text='There are 1337 IOTABOTS, 1001 SPACEBOTS, 3333 SOONABOTS & 999 RUNBOTS'
           headline='How many NFTs exist right now?'
-        >
-          {}
-        </ContentBox>
-        <ContentBox
-          image='http://assets.iotabots.io/compressed/990.png'
-          // eslint-disable-next-line max-len
-          text='1337 unique IOTABOTS, 1001 SPACEBOTS, 3333 SOONABOTS.'
-          headline='How many NFTs will exist?'
         >
           {}
         </ContentBox>
@@ -51,16 +43,8 @@ const FAQ: React.FC = () => (
         <ContentBox
           image='http://assets.iotabots.io/compressed/845.png'
           // eslint-disable-next-line max-len
-          text='Download your favourite IOTABOT or SOONABOT and use it as a profile picture. The SOONABOT is a fully animated BOT. Your SPACEBOT gives you access to a NFT game.'
+          text='Download your favourite BOT and use it as a profile picture. Your SPACEBOT and RUNBOT gives you access to a NFT game.'
           headline='What can I do with my BOTs?'
-        >
-          {}
-        </ContentBox>
-        <ContentBox
-          image=''
-          // eslint-disable-next-line max-len
-          text='Use the following link for the instructions to download the animated version of your SOONABOT: http://docs.iotabots.io/faq#soonabots '
-          headline='How do I get the animated SOONABOT version as my profile picture?'
         >
           {}
         </ContentBox>
